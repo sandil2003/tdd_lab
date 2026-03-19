d335b28b1d139a8f3f1caf0de3d7a620278817d5
sandil2003 <it23657700@my.sliit.lk>
2026-03-19 09:42:10 +0530

    refractor: create type hints

--- src/catalog.py
+++ src/catalog.py
@@ -1,15 +1,21 @@
 class Product:
-    def __init__(self, sku, name, price):
+    """Represents a product in the catalog."""
+    def __init__(self, sku: str, name: str, price: float):
         if price < 0:
             raise ValueError("Price cannot be negative")
-        self.sku, self.name, self.price = sku, name, price
+        self.sku: str = sku
+        self.name: str = name
+        self.price: float = price
 
 class Catalog:
+    """An inventory system to manage and retrieve products by SKU."""
     def __init__(self):
         self.products = {}
 
-    def add_product(self, product):
+    def add_product(self, product: Product):
+        """Adds a product to the catalog."""
         self.products[product.sku] = product
 
-    def get_by_sku(self, sku):
+    def get_by_sku(self, sku: str) -> Product | None:
+        """Retrieves a product if found otherwise None"""
         return self.products.get(sku)
