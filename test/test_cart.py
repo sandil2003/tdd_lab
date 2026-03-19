cf44702751b17f65cf270645a9fa475c08b2a4b4
sandil2003 <it23657700@my.sliit.lk>
2026-03-19 09:51:26 +0530

    test: failing test for the cart

---
+++ test/test_cart.py
@@ -0,0 +1,11 @@
+import pytest
+from src.catalog import Catalog, Product
+
+def test_cart_total():
+    cat = Catalog()
+    p = Product("A", "Apple", 2.0)
+    cat.add_product(p)
+
+    cart = Cart(cat)
+    cart.add_item("A", 3)
+    assert cart.get_total() == 6.0
